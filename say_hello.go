41f3e49b9c4aa247b0805d834c25c46fa6979fe1
Hernawan Abidin <97391283+Herdi1@users.noreply.github.com>
2025-12-19 13:30:22 +0700

    update say_hello with params

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World! Welcome to The New World"
+func SayHello(name string) string {
+	return "Hello World! Welcome to The New World " + name
 }
